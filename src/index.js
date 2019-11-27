79dbdabe538c2edbf771476966c06e9d7ddc9d9b
Kleiton Marques <kleiton.88design@gmail.com>
2019-11-27 19:45:07 -0200

    Primeiro componente

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PrimeiroComponente from './componentes/PrimeiroComponente'
 
 const elemento = document.getElementById("root")
-ReactDOM.render(<h1>Ola React</h1>, elemento)
+ReactDOM.render(
+    <div>
+        <PrimeiroComponente />
+    </div>
+, elemento)
