fde6169a11fb3eaabd7c66999735820e1cfffaaa
Kleiton Marques <kleiton.88design@gmail.com>
2019-12-01 17:29:03 -0200

    Primeiro componente ArrowFunction

--- src/componentes/PrimeiroComponente.jsx
+++ src/componentes/PrimeiroComponente.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
-export default function() {
-    return <h1>Primeiro Componente!</h1>
-}
+export default () =>
+    <div>
+        <h1>Primeiro Componente (Arrow)!</h1>
+    </div>
