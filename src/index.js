3a0fcc520c7081b918d663de69d16a3105441c83
Kleiton Marques <kleiton.88design@gmail.com>
2019-12-03 20:23:29 -0200

    Sistema de modulos --- import/export

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
-// import { CompA, CompB as B } from './componentes/DoisComponentes'
-import MultiElementos from './componentes/MultiElementos'
+import A, { CompB as B } from './componentes/DoisComponentes'
+// import MultiElementos from './componentes/MultiElementos'
 
 
 const elemento = document.getElementById("root")
 ReactDOM.render(
     <div>
-        <MultiElementos />
+        <A valor="Olá eu sou A!" />
+        <B valor="B na área!" />
     </div>
 , elemento)
