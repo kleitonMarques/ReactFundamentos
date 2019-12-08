bf5b15557efa3d359cc6ead68074ffb4d60fe82b
Kleiton Marques <kleiton.88design@gmail.com>
2019-12-08 17:00:04 -0200

    Relação entre componentes #02

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
 // import A, { CompB as B } from './componentes/DoisComponentes'
 // import MultiElementos from './componentes/MultiElementos'
-import FamiliaSilva from './componentes/FamiliaSilva'
+// import FamiliaSilva from './componentes/FamiliaSilva'
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
 
 
 const elemento = document.getElementById("root")
 ReactDOM.render(
     <div>
-        <FamiliaSilva />
+        <Familia sobrenome="Pereira">
+            <Membro nome="Andre" />
+            <Membro nome="Mariana" />
+        </Familia>       
+        {/* <FamiliaSilva /> */}
         {/* <A valor="Olá eu sou A!" /> */}
         {/* <B valor="B na área!" /> */}
     </div>
