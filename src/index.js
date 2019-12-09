fb70c97a5e1d3951fcb9d0323ed70dfe9113c816
Kleiton Marques <kleiton.88design@gmail.com>
2019-12-09 20:44:22 -0200

    Propagando props para componentes filhos #01

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
 // import A, { CompB as B } from './componentes/DoisComponentes'
 // import MultiElementos from './componentes/MultiElementos'
-import FamiliaSilva from './componentes/FamiliaSilva'
+// import FamiliaSilva from './componentes/FamiliaSilva'
 import Familia from './componentes/Familia'
 import Membro from './componentes/Membro'
 
@@ -13,9 +13,9 @@ ReactDOM.render(
     <div>
         <Familia sobrenome="Pereira">
             <Membro nome="Andre" />
-            <Membro nome="Mariana" />
+            {/* <Membro nome="Mariana" /> */}
         </Familia>       
-        <FamiliaSilva />
+        {/* <FamiliaSilva /> */}
         {/* <A valor="Olá eu sou A!" /> */}
         {/* <B valor="B na área!" /> */}
     </div>
