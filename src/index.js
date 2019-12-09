8f14e49c7f5caa9fbb4e607f25e339d1e60d46c4
Kleiton Marques <kleiton.88design@gmail.com>
2019-12-09 20:29:06 -0200

    Comunicação direta entre componentes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
 // import A, { CompB as B } from './componentes/DoisComponentes'
 // import MultiElementos from './componentes/MultiElementos'
-// import FamiliaSilva from './componentes/FamiliaSilva'
+import FamiliaSilva from './componentes/FamiliaSilva'
 import Familia from './componentes/Familia'
 import Membro from './componentes/Membro'
 
@@ -15,7 +15,7 @@ ReactDOM.render(
             <Membro nome="Andre" />
             <Membro nome="Mariana" />
         </Familia>       
-        {/* <FamiliaSilva /> */}
+        <FamiliaSilva />
         {/* <A valor="Olá eu sou A!" /> */}
         {/* <B valor="B na área!" /> */}
     </div>
