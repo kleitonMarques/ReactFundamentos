681d7c364b7ba967baa8c17cfc744fd04a4725c8
Kleiton Marques <kleiton.88design@gmail.com>
2019-12-11 21:23:27 -0200

    Propagando props para os componentes filhos #02

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
     <div>
         <Familia sobrenome="Pereira">
             <Membro nome="Andre" />
-            {/* <Membro nome="Mariana" /> */}
+            <Membro nome="Mariana" />
         </Familia>       
         {/* <FamiliaSilva /> */}
         {/* <A valor="Olá eu sou A!" /> */}
